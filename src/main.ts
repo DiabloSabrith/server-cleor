f8ca6c3452e0ae62fa295f2a43b9d22cad49b032
abc <abc@example.com>
2025-07-11 16:37:25 +0500

    fix: add port log to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,22 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Разрешаем CORS
   app.enableCors();
+
+  // Подключаем папку со статикой (загрузка файлов)
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
   });
-  await app.listen(process.env.PORT || 3000);
+
+  // Получаем порт из переменных окружения (для Timeweb) или используем 3000
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  // Добавляем лог, чтобы в Timeweb было видно, что сервер работает
+  console.log(`✅ Cleor Server is running on http://localhost:${port}`);
 }
+
 bootstrap();
-/* Тоесть  по умолчанию создается INest =Aplications и поэтому некотрые вещи н еработают 
- а тут мы как дженерик прокидываем дурое созадени приложения NestExpressApplication
-*/
