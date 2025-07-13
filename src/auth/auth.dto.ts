e0b20003e64773c8a4b681295a6db1cada8ad970
abc <abc@example.com>
2025-07-13 20:10:36 +0500

    !

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,13 +1,13 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 export class AuthDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @MinLength(2, {
     message: 'Длина email должна быть не меньше 2 символов',
   })
   @IsString()
-  name: string;
+  name!: string;
   @IsString()
-  password: string;
+  password!: string;
 }
