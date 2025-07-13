f179dcb246be6091729a1bb7e8cc601a7d45459c
abc <abc@example.com>
2025-07-13 23:42:27 +0500

    __dirname, '..', '..', 'uploads'

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
     app.enableCors();
 
-    app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    app.useStaticAssets(join(__dirname, '..', '..', 'uploads'), {
       prefix: '/uploads',
     });
 
