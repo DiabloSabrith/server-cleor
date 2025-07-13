353d8cf341be17897160643409913167de914ed7
abc <abc@example.com>
2025-07-14 01:28:10 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-    app.enableCors();
+    app.enableCors(
+      origin:"*"
+    );
 
     app.useStaticAssets(join(__dirname, '..', '..', 'uploads'), {
       prefix: '/uploads',
